135564a8aeddf3b833f95dc80ce7d5dac8d64cec
OZ-tro <odzm302@gmail.com>
2024-08-11 23:24:30 +0000

    Generador de excusas OZtro

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".excusas").innerHTML = excuse();
 };
+
+function excuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when i was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let subject = Math.floor(Math.random() * who.length);
+  let verb = Math.floor(Math.random() * action.length);
+  let object = Math.floor(Math.random() * what.length);
+  let location = Math.floor(Math.random() * when.length);
+
+  return (
+    who[subject] +
+    " " +
+    action[verb] +
+    " " +
+    what[object] +
+    " " +
+    when[location]
+  );
+}
